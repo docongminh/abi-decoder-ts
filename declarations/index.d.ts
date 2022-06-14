44b4e38bf42fbe31cbf1cae86ba421c3d964bd79
docongminh <cristiando0902@gmail.com>
2022-06-14 09:00:59 +0700

    UPDATE: update config

---
+++ declarations/index.d.ts
@@ -0,0 +1,11 @@
+export declare function getABIs(): any[];
+export declare function typeToString(input: any): any;
+export declare function addABI(abiArray: any): void;
+export declare function removeABI(abiArray: any): void;
+export declare function getMethodIDs(): {};
+export declare function decodeMethod(data: any): {
+    name: any;
+    params: any[];
+};
+export declare function decodeLogs(logs: any): any;
+//# sourceMappingURL=index.d.ts.map
